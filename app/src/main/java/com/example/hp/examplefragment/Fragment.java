45b390bd9af9946ca31e6224ca3373dd519fea9a
Siddhartha1234 <es15btech11018@iith.ac.in>
2016-06-04 21:14:04 +0530

    ListFragment simple

--- app/src/main/java/com/example/hp/examplefragment/Fragment.java
+++ app/src/main/java/com/example/hp/examplefragment/Fragment.java
@@ -20,9 +20,7 @@ public class Fragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                "Linux", "OS/2" };
+        String[] values = new String[] {"Mango","Orange","Guava"};
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
